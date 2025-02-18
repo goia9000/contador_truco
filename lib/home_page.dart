b334e8949f6db69a86bcfd4d37cf2d2fad6608e7
Bruno <brunogoia92@gmail.com>
2025-02-18 16:48:29 -0300

    Adicionamos widgets e completamos metade da tela do "contador_truco"

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,20 +12,207 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Contador de Truco",
-          style: TextStyle(fontWeight: FontWeight.w400),
-        ),
-        backgroundColor: const Color(0xff7D6B7D),
+        title: const Text("Contador de Truco",
+            style: TextStyle(fontWeight: FontWeight.w400)),
+        backgroundColor: const Color(0xFF7D6B7D),
         centerTitle: true,
         foregroundColor: Colors.white,
       ),
-      body:const Column(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(children: [
-            Column(),
-            Column()
-          ],)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Nós",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF7D6B7D)),
+                  ),
+                  Container(
+                    width: 120,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFFFFFDC),
+                        border: Border.all(
+                            width: 1,
+                            style: BorderStyle.solid,
+                            color: Colors.black45),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Text(
+                      "1",
+                      style: TextStyle(fontSize: 60, color: Colors.red),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffe5e5e5),
+                      shape: BoxShape.circle,
+                      border:
+                          Border.all(width: 2, color: const Color(0xff707070)),
+                    ),
+                    child: const Text(
+                      '-1',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF707070)),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF7d6B7D),
+                      minimumSize: const Size(120, 60),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text(
+                      "+1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff7d6b7d),
+                        minimumSize: const Size(120, 60),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      child: const Text(
+                        "+3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      )),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF7d6B7D),
+                      minimumSize: const Size(120, 60),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text(
+                      "+6",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text(
+                    "Eles",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF7D6B7D)),
+                  ),
+                  Container(
+                    width: 120,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFFFFFDC),
+                        border: Border.all(
+                            width: 1,
+                            style: BorderStyle.solid,
+                            color: Colors.black45),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Text(
+                      "6",
+                      style: TextStyle(fontSize: 60, color: Colors.red),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffe5e5e5),
+                      shape: BoxShape.circle,
+                      border:
+                          Border.all(width: 2, color: const Color(0xff707070)),
+                    ),
+                    child: const Text(
+                      '-1',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF707070)),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF7d6B7D),
+                      minimumSize: const Size(120, 60),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text(
+                      "+1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff7d6b7d),
+                        minimumSize: const Size(120, 60),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      child: const Text(
+                        "+3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      )),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF7d6B7D),
+                      minimumSize: const Size(120, 60),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text(
+                      "+6",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
